7ea8b17a4f8eb0cdd7a9e5cef5bb8adf21f8f3fc
czine201 <94713530+czine201@users.noreply.github.com>
2024-04-08 16:45:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const arrow = document.querySelectorAll(".arrow");
-const bookletLists = document.querySelectorAll(".booklet-list");
+const arrows = document.querySelectorAll('.arrow');
+const bookletList = document.querySelectorAll('.booklet-list');
 
-arrow.forEach((arrow, i)=>{
-arrow.addEventListener("click", ()=>{
-console.log("you clicked to: " +i)
-})
-})
+arrows.forEach((arrow, i) => {
+  const itemNumber = bookletList[i].querySelectorAll('img').length;
+  let clickCounter = 0;
+  arrow.addEventListener('click', () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      bookletList[i].style.transform = `translateX(${-300 * clickCounter}px)`;
+    } else {
+      bookletList[i].style.transform = 'translateX(0)';
+      clickCounter = 0;
+    }
+  });
+});
+//booklist
+document.getElementById('search-icon').addEventListener('click', function() {
+        var bookList = document.getElementById('book-list');
+        if (bookList.style.display === 'none') {
+            bookList.style.display = 'block';
+        } else {
+            bookList.style.display = 'none';
+        }
+    });
+
